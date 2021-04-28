1324866613af2169934f12d102c9cff4747d6e6b
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2021-04-27 21:47:42 -0400

    completed

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const db = require("./db");
+const Brand = require("./models/brand");
+const Product = require("./models/product");
+
+db.on("error", console.error.bind(console, "MongoDB Connection Error!"));
+
+app.listen(PORT, () => {
+  console.log(`Express seerver listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("This is the root");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      throw Error("Product not found.");
+    }
+    res.json(product);
+  } catch (e) {
+    console.log(e);
+    res.send();
+  }
+});
+
+app.get("/brands", async (req, res) => {
+  const brands = await Brand.find();
+  res.json(brands);
+});
+
+app.get("/brands/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const brand = await Brand.findById(id);
+    if (!brand) {
+      throw Error("Brand not found.");
+    }
+    res.json(brand);
+  } catch (e) {
+    console.log(e);
+    res.send();
+  }
+});
